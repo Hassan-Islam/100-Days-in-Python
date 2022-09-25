ab6b84def67e5628b8d885a781e6b433f88ff0eb
Hassan-Islam <hassananxari4@gmail.com>
2022-09-25 11:39:44 +0500

    Day 1 completed

--- Day001/input.py
+++ Day001/input.py
@@ -2,9 +2,3 @@
 input ("what is your name?")
 #print function
 print("Hi" + " "+ input ("what is your name?"))
-
-
-
-
-
-
--- Day1-project/Band_name_generator.py
+++
@@ -1,8 +0,0 @@
-#Create a greeting for your program
-print ("Welcome to the band name generator")
-#Ask the user for the city that they grew up in.
-city = input("Which city did you grew up in\n")
-#Ask the user for the name of a pet.
-pet = input("What is the name of a pet?\n")
-#combine the name of their city and pet and show them their band name.
-print ("Your band name could be " + city + " " + pet)
